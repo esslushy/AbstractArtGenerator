35edbbe6a49e77f9deab80031914576ffa915ae8
Slushy <ess090502@gmail.com>
2019-01-08 16:42:19 -0600

    fixed naming schemes

--- src/ArtGenerator.py
+++ src/ArtGenerator.py
@@ -40,19 +40,19 @@ tf.set_random_seed(7)
 #takes image x and ouputs a value between 0 and 1 where 0 is fake and 1 is real
 def discriminator(x):#might be too powerful, already lowered learning rate, but might need to add dropout also
     with tf.variable_scope("discriminator", reuse=tf.AUTO_REUSE):
-        with tf.variable_scope("convolutional_layer_1"):
+        with tf.variable_scope("resize_convolutional_layer_1"):
             x = resizeConvolutLayer(x, 128, 32)#3x64x64 -> 128x32x32
             #don't batch normalize in first layer of discriminator
             x = nn.leaky_relu(x, alpha=0.2)
-        with tf.variable_scope("convolutional_layer_2"):
+        with tf.variable_scope("resize_convolutional_layer_2"):
             x = resizeConvolutLayer(x, 256, 16)#128x32x32 -> 256x16x16
             x = layers.batch_normalization(x, training=True)
             x = nn.leaky_relu(x, alpha=0.2)
-        with tf.variable_scope("convolutional_layer_3"):
+        with tf.variable_scope("resize_convolutional_layer_3"):
             x = resizeConvolutLayer(x, 512, 8)#256x16x16 -> 512x8x8
             x = layers.batch_normalization(x, training=True)
             x = nn.leaky_relu(x, alpha=0.2)
-        with tf.variable_scope("convolution_layer_4"):
+        with tf.variable_scope("resize_convolution_layer_4"):
             x = resizeConvolutLayer(x, 1024, 4)#512x8x8 -> 1024x4x4
             x = layers.batch_normalization(x, training=True)
             x = nn.leaky_relu(x, alpha=0.2)
@@ -68,19 +68,19 @@ def generator(z):
         with tf.variable_scope("reshape_and_flatten"):
             z = layers.dense(inputs=z, units=4*4*1024)#flatten
             z = tf.reshape(z, (-1, 4, 4, 1024))#reshape noise 
-        with tf.variable_scope("deconvolutional_layer_1"):
+        with tf.variable_scope("resize_convolution_layer_1"):
             z = resizeConvolutLayer(z, 512, 8)#1024x4x4 -> 512x8x8
             z = layers.batch_normalization(z, training=True)
             z = nn.relu(z)
-        with tf.variable_scope("deconvolutional_layer_2"):
+        with tf.variable_scope("resize_convolution_layer_2"):
             z = resizeConvolutLayer(z, 256, 16)#512x8x8 -> 256x16x16
             z = layers.batch_normalization(z, training=True)
             z = nn.relu(z)
-        with tf.variable_scope("deconvolutional_layer_3"):
+        with tf.variable_scope("resize_convolution_layer_3"):
             z = resizeConvolutLayer(z, 128, 32)#256x16x16 -> 128x32x32
             z = layers.batch_normalization(z, training=True)
             z = nn.relu(z)
-        with tf.variable_scope("deconvolutional_layer_4"):
+        with tf.variable_scope("resize_convolution_layer_4"):
             z = resizeConvolutLayer(z, 3, 64)#128x32x32 -> 3x64x64
             #no batch normalization in last layer of generatror
             #don't use relu for output
@@ -151,7 +151,7 @@ with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
     generatorOptimizer = tf.train.AdamOptimizer(0.0002).minimize(generatorLoss, var_list=gTrainableVariables)
 
 #config for session with multithreading, but limit to 3 of my 4 CPUs (tensor uses all by default: https://stackoverflow.com/questions/38836269/does-tensorflow-view-all-cpus-of-one-machine-as-one-device)
-config = tf.ConfigProto(intra_op_parallelism_threads=2, inter_op_parallelism_threads=1, allow_soft_placement=True, log_device_placement=True)
+config = tf.ConfigProto(intra_op_parallelism_threads=2, inter_op_parallelism_threads=1, allow_soft_placement=True)
 
 #Saver for when stuff goes wrong
 saver = tf.train.Saver()
