af5fd5bdccb13527c291542b31ff932783f25e00
esslushy <esslushy@gmail.com>
2019-03-31 11:42:15 -0500

    updated labels, but doing internet is proving problematic will change to something else

--- srcv3/ArtGeneratorWithTags.py
+++ srcv3/ArtGeneratorWithTags.py
@@ -1,19 +1,16 @@
 #imports
-#load data
-from torch.utils.data import dataloader, TensorDataset
-import torch
 #network
 import tensorflow as tf
 import numpy as np
 from tensorflow.python.keras.layers import Dense, BatchNormalization
-from tensorflow import nn
+from tensorflow import nn, layers
 
 import cv2
 import urllib
 
 from ops import noise, deconvolutLayer, convolutLayer, convolutionalConcat
 
-batchSize = 128
+batchSize = 32
 noiseLength = 100
 tagLength = 20
 numEpochs = 150
@@ -22,14 +19,13 @@ tf.set_random_seed(7)
 #global step
 globalStep = 1
 #dataset
-images = torch.from_numpy(np.load('../dataset/images.npy'))
-tags = torch.from_numpy(np.load('../dataset/tags.npy'))
-dataset = TensorDataset(images, tags)
-dataLoader = dataloader.DataLoader(dataset, batch_size=batchSize, shuffle=True)
+images = np.load('../dataset/images.npy')
+tags = np.load('../dataset/tags.npy')
+dataset = tf.data.Dataset.from_tensor_slices((images, tags)).batch(batchSize)
 
 def getImage(link):
     try:
-        resp = urllib.request.urlopen(link)#get data
+        resp = urllib.request.urlopen(link.decode('ASCII'))#get data
         img = np.asarray(bytearray(resp.read()), dtype="uint8")#read as image
         img = cv2.imdecode(img, cv2.IMREAD_COLOR)#cv1 image
     except urllib.error.HTTPError as e:#sometimes pages are missing or server won't respond this is ok as we will still get enough data
@@ -49,11 +45,10 @@ def getImage(link):
     np.divide(img, np.array(127.5), out=img)
     return img
 
-
 #models
 def generator(z, y):
     yb = tf.reshape(y, [-1, 1, 1, tagLength])#makes 3d config of tags to append to conv
-    with tf.variable_scope('generator'):
+    with tf.variable_scope('generator', reuse=tf.AUTO_REUSE):
         with tf.variable_scope('reshape_and_flatten'):
             z = tf.concat(values=[z, y], axis=1)#connect along channels not batch
             z = Dense(units=1024*4*4, kernel_initializer=tf.contrib.layers.xavier_initializer())(z)
@@ -61,52 +56,51 @@ def generator(z, y):
         with tf.variable_scope('deconvolution_layer_1'):
             z = convolutionalConcat(z, yb)
             z = deconvolutLayer(z, 512)#4x4x1024 -> 8x8x512
-            z = BatchNormalization(axis=3)(z)#3 is channels axis
+            z = layers.batch_normalization(z,training=True)#3 is channels axis
             z = nn.relu(z)
         with tf.variable_scope('deconvolution_layer_2'):
             z = convolutionalConcat(z, yb)
             z = deconvolutLayer(z, 256)#8x8x512 -> 16x16x256
-            z = BatchNormalization(axis=3)(z)
+            z = layers.batch_normalization(z,training=True)
             z = nn.relu(z)
         with tf.variable_scope('deconvolution_layer_3'):
             z = convolutionalConcat(z, yb)
             z = deconvolutLayer(z, 128)#16x16x256 -> 32x32x128
-            z = BatchNormalization(axis=3)(z)
+            z = layers.batch_normalization(z,training=True)
             z = nn.relu(z)
         with tf.variable_scope('deconvolution_layer_4'):
             z = convolutionalConcat(z, yb)
             z = deconvolutLayer(z, 64)#32x32x128 -> 64x64x64
-            z = BatchNormalization(axis=3)(z)
+            z = layers.batch_normalization(z,training=True)
             z = nn.relu(z)
         with tf.variable_scope('deconvolution_layer_5'):
             z = convolutionalConcat(z, yb)
-            z = deconvolutLayer(z, 32, stride=1)#64x64x64 -> 64x64x32
-            z = BatchNormalization(axis=3)(z)
+            z = deconvolutLayer(z, 32)#64x64x64 -> 128x128x32
+            z = layers.batch_normalization(z,training=True)
             z = nn.relu(z)
         with tf.variable_scope('deconvolution_layer_6'):
             z = convolutionalConcat(z, yb)
-            z = deconvolutLayer(z, 16, stride=1)#64x64x32 -> 64x64x16
-            z = BatchNormalization(axis=3)(z)
+            z = deconvolutLayer(z, 16)#128x128x32 -> 256x256x16
+            z = layers.batch_normalization(z,training=True)
             z = nn.relu(z)
         with tf.variable_scope('deconvolution_layer_7'):
             z = convolutionalConcat(z, yb)
-            z = deconvolutLayer(z, 8, stride=1)#64x64x16 -> 64x64x8
-            z = BatchNormalization(axis=3)(z)
+            z = deconvolutLayer(z, 8, stride=1)#256x256x16 -> 256x256x8
+            z = layers.batch_normalization(z,training=True)
             z = nn.relu(z)
         with tf.variable_scope('deconvolution_layer_8'):
             z = convolutionalConcat(z, yb)
-            z = deconvolutLayer(z, 4, stride=1)#64x64x8 -> 64x64x4
-            z = BatchNormalization(axis=3)(z)
+            z = deconvolutLayer(z, 4, stride=1)#256x256x8 -> 256x256x4
+            z = layers.batch_normalization(z,training=True)
             z = nn.relu(z)
         with tf.variable_scope('deconvolution_layer_9'):
             z = convolutionalConcat(z, yb)
-            z = deconvolutLayer(z, 3, stride=1)#64x64x4 -> 64x64x3
-            print(z)
+            z = deconvolutLayer(z, 3, stride=1)#256x256x4 -> 256x256x3
             #no relu or batch norm for output
         with tf.variable_scope('output'):
             out = nn.tanh(z)
             #output to show it off
-            tf.summary.image('Generated Images', out, max_outputs=48)
+            tf.summary.image('Generated Images', out, max_outputs=16)
     return out
 
 def discriminator(x, y):
@@ -114,27 +108,27 @@ def discriminator(x, y):
     with tf.variable_scope('discriminator', reuse=tf.AUTO_REUSE):
         with tf.variable_scope('convolutional_layer_1'):
             x = convolutionalConcat(x, yb)
-            x = convolutLayer(x, 4, stride=1)#64x64x3 -> 64x64x4
+            x = convolutLayer(x, 4, stride=1)#256x256x3 -> 256x256x4
             x = BatchNormalization(axis=3)(x)
             x = nn.leaky_relu(x, alpha=0.2)
         with tf.variable_scope('convolutional_layer_2'):
             x = convolutionalConcat(x, yb)
-            x = convolutLayer(x, 8, stride=1)#64x64x4 -> 64x64x8
+            x = convolutLayer(x, 8, stride=1)#256x256x4 -> 256x256x8
             x = BatchNormalization(axis=3)(x)
             x = nn.leaky_relu(x, alpha=0.2)
         with tf.variable_scope('convolutional_layer_3'):
             x = convolutionalConcat(x, yb)
-            x = convolutLayer(x, 16, stride=1)#64x64x8 -> 64x64x16
+            x = convolutLayer(x, 16, stride=1)#256x256x8 -> 256x256x16
             x = BatchNormalization(axis=3)(x)
             x = nn.leaky_relu(x, alpha=0.2)
         with tf.variable_scope('convolutional_layer_4'):
             x = convolutionalConcat(x, yb)
-            x = convolutLayer(x, 32, stride=1)#64x64x16 -> 64x64x32
+            x = convolutLayer(x, 32)#256x256x16 -> 128x128x32
             x = BatchNormalization(axis=3)(x)
             x = nn.leaky_relu(x, alpha=0.2)
         with tf.variable_scope('convolutional_layer_5'):
             x = convolutionalConcat(x, yb)
-            x = convolutLayer(x, 64, stride=1)#64x64x32 -> 64x64x64
+            x = convolutLayer(x, 64)#128x128x32 -> 64x64x64
             x = BatchNormalization(axis=3)(x)
             x = nn.leaky_relu(x, alpha=0.2)
         with tf.variable_scope('convolutional_layer_6'):
@@ -166,7 +160,7 @@ def discriminator(x, y):
     return out, logits
 
 #placeholders
-x = tf.placeholder(dtype=tf.float32, shape=(None, 64, 64, 3), name='Images')
+x = tf.placeholder(dtype=tf.float32, shape=(None, 256, 256, 3), name='Images')
 z = tf.placeholder(dtype=tf.float32, shape=(None, noiseLength), name='Noise')
 y = tf.placeholder(dtype=tf.float32, shape=(None, tagLength), name='Tags')
 
@@ -180,13 +174,15 @@ with tf.device(device):
     discriminatorReal, discriminatorRealLogits = discriminator(x, y)#make discriminator that takes data from the real
     discriminatorFake, discriminatorFakeLogits = discriminator(generatorSamples, y)#make discriminator that takes fake data
 
-discriminatorLoss = tf.reduce_mean(
+discriminatorLossReal = tf.reduce_mean(
                            nn.sigmoid_cross_entropy_with_logits(
                                logits=discriminatorRealLogits, labels=tf.ones_like(discriminatorRealLogits),
                                name="discriminator_loss_real"
                                #takes real input and makes the labels 1 or real because it wants to identify real data as real
                            )
-                    ) + tf.reduce_mean(
+                    ) 
+                    
+discriminatorLossFake = tf.reduce_mean(
                            nn.sigmoid_cross_entropy_with_logits(
                                logits=discriminatorFakeLogits, labels=tf.zeros_like(discriminatorFakeLogits),
                                name="discriminator_loss_fake"
@@ -201,6 +197,9 @@ generatorLoss = tf.reduce_mean(
                                #takes fake input and makes the labels 0 or fake because it wants to identify fake data as fake
                             )
                         )
+
+discriminatorLoss = discriminatorLossFake + discriminatorLossReal
+
 #write losses to tensorboard
 tf.summary.scalar("Discriminator Total Loss", discriminatorLoss)
 tf.summary.scalar("Generator Loss", generatorLoss)
@@ -234,24 +233,27 @@ with tf.Session(config=config) as sess:
     sess.run(tf.global_variables_initializer())
     print("Starting Session")
     for epoch in range(numEpochs):
-        for numBatch, (links, tempTags) in enumerate(dataLoader):
-
-            links = links[0].numpy() #turns them into numpy and sticks them into another array
-            tempTags = tempTags[0].numpy()
-            #gets images from link
-            images = []
-            tags = []
-            for i in range(links):
-                img = getImage(links[i])
-                if(img == None):
-                    continue
-                images.append(img)
-                tags.append(tempTags[i])
-            summary, _, _ = sess.run([merged, generatorOptimizer, discriminatorOptimizer], feed_dict={ x : images, z : noise(len(images), noiseLength),  y : tags })
-            if numBatch % 10 == 0:
+        nextImages, nextTags = dataset.make_one_shot_iterator().get_next()
+        print('iterator ready')
+        while(True):#run constantly
+            try:
+                links = sess.run(nextImages)[0] #turns them into numpy and sticks them into another array
+                tempTags = sess.run(nextTags)[0]
+                #gets images from link
+                images = []
+                tags = []
+                for i in range(len(links)):
+                    img = getImage(links[i])
+                    if(img == None):
+                        continue
+                    images.append(img)
+                    tags.append(tempTags[i])
+                summary, _, _ = sess.run([merged, generatorOptimizer, discriminatorOptimizer], feed_dict={ x : images, z : noise(len(images), noiseLength),  y : tags })
                 writer.add_summary(summary, globalStep)
                 globalStep+=1
-            if numBatch % 100 == 0:
-                saver.save(sess, "./model/DCGAN_Epoch_%s_Batch_%s.ckpt" % (epoch, numBatch))
+            except tf.errors.OutOfRangeError:#when data runs out
+                print('Finished Epoch ' + epoch)
+                saver.save(sess, "./model/DCGAN_Epoch_%s.ckpt" % (epoch))
+                break
     saver.save(sess, './model/FullyTrainedModel')          
     writer.close()
